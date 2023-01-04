9b6aff73532696164cbb2b5f08f4fef64e850df4
plagaz <plagaz66@gmail.com>
2023-01-04 12:12:40 -0400

    2. Primeros pasos y custom hook y 3. Componente de Input

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import useFormulario from "./hooks/useFormulario";
+import Input from "./components/Input";
+
+function App() {
+  const [formulario, handleChange] = useFormulario({ name: "", lastname: "" });
+  console.log(formulario);
+  return (
+    <form>
+      <Input
+        label="Nombre"
+        name="name"
+        value={formulario.name}
+        onChange={handleChange}
+      />
+      <Input
+        label="Apellido"
+        name="lastname"
+        value={formulario.lastname}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
+
+export default App;
